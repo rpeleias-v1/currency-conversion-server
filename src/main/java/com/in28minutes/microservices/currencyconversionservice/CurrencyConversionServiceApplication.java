96f11d6224f3719bf0fb8ea2edc466de86f75659
Rodrigo Peleias <rodrigo.peleias@clickbus.com>
2019-03-27 14:09:49 -0300

    Added spring eureka and zuul

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,16 +1,24 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients("com.in28minutes.microservices.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 
 }
--- src/main/java/com/in28minutes/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,6 +2,7 @@ package com.in28minutes.microservices.currencyconversionservice.controller;
 
 import com.in28minutes.microservices.currencyconversionservice.entity.CurrencyConversionBean;
 import com.in28minutes.microservices.currencyconversionservice.proxy.CurrencyExchangeServiceProxy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class CurrencyConversionController {
 
@@ -48,6 +50,8 @@ public class CurrencyConversionController {
 
 
         CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+        log.info("{}", response);
 
         return new CurrencyConversionBean(response.getId(),
                 from, to,
